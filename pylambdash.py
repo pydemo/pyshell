c018da4abdf6aa3a4d0c7b7ec30fa09cceb8f809
pydemo <36740585+pydemo@users.noreply.github.com>
2020-11-10 10:25:14 -0500

    Update pylambdash.py

--- pylambdash.py
+++ pylambdash.py
@@ -9,19 +9,21 @@ from pprint import pprint as pp
 client = boto3.client('lambda', region_name='us-east-2')
 response = client.invoke(
     InvocationType='RequestResponse',
-    #FunctionName=os.environ['LAMBDASH_FUNCTION'] or 'lambdash',
-    FunctionName='pyshell',
+    #FunctionName=os.environ['LAMBDASH_FUNCTION'] or 'pylambdash',
+    FunctionName='pylambdash',
     Payload='{"command":'+json.dumps(" ".join(sys.argv[1:]))+'}')
 result = json.load(response['Payload'])
-#pp(result['stdout'])
 for line in result['stdout'].split(r'\n'):
     os.write(sys.stdout.fileno(), line.encode('utf-8'))
-#os.write(sys.stderr.fileno(), base64.b64decode(result['stderr']))
+
 if result['stderr']:
     print('#'*80)
     for line in result['stderr'].split(r'\n'):
         os.write(sys.stderr.fileno(), line.encode('utf-8'))
     print('#'*80)
+    
 if result['error'] and 'code' in result['error']:
+    print('FAILURE')
     exit(result['error']['code'])
 exit(0)
+
