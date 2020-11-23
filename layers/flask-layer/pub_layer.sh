b4c911d1646bab667633de04cc4f111dfc8c8e99
pydemo <alex_buz@yahoo.com>
2020-11-23 11:51:32 -0500

    new

--- layers/flask-layer/pub_layer.sh
+++
@@ -1,5 +0,0 @@
-aws lambda publish-layer-version \
-    --layer-name "flask-layer" \
-    --description "Lambda Layer for Flask 1.1.1" \
-    --zip-file "fileb://flask-layer.zip" \
-    --compatible-runtimes "python3.7"
